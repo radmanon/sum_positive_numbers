ddd3f324aafc704713a47350852b50236807f9f9
radmanon <97783226+radmanon@users.noreply.github.com>
2023-01-02 22:11:48 -0800

    sum_positive_numbers is added successfully.

---
+++ sum_positive_numbers.py
@@ -0,0 +1,19 @@
+#using recursion
+def sum_positive_numbers(n):
+  if n <= 1:
+      return n
+  return n + sum_positive_numbers(n-1)
+
+print(sum_positive_numbers(3)) # Should be 6
+print(sum_positive_numbers(5)) # Should be 15
+
+
+#using normal functions
+def sum_positive_numbers(n):
+  sums = 0
+  for v in range(1,n+1):
+    sums += v
+  return sums
+
+print(sum_positive_numbers(3)) # Should be 6
+print(sum_positive_numbers(5)) # Should be 15
